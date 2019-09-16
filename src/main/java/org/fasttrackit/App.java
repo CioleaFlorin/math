7e431702bcb54ab875c636776bced9ba1c7f3381
Florin <ciolea.florin.99@gmail.com>
2019-09-16 18:29:44 +0300

    update

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,13 +8,13 @@ public class App
 {
     public static void main( String[] args ){
         Addition addition = new Addition();
-        //System.out.println("Addition: " + addition.sum(2.0, 2.0));
+        System.out.println("Addition: " + addition.sum(2.0, 2.0));
 
         Subtraction subtraction=new Subtraction();
-        //System.out.println("Subtraction: " + subtraction.subtraction(3,1));
+        System.out.println("Subtraction: " + subtraction.subtraction(3,1));
 
         Multiplication multiplication=new Multiplication();
-        //System.out.println("Multiplication: " + multiplication.multiplication(3,0));
+        System.out.println("Multiplication: " + multiplication.multiplication(3,0));
 
 
 
